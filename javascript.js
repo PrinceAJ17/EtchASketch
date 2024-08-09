a0a9ab561d4e9cdd8f7036b83b1eae4f61317af7
PrinceAJ17 <princeajohn2005@gmail.com>
2024-08-09 21:17:07 +0100

    Changed up some alignment

--- javascript.js
+++ javascript.js
@@ -8,11 +8,11 @@ numinpt.addEventListener("change", () => {
     for(let j=0; j<gridSize; j++){
         const div1 = document.createElement("div")
         div1.classList.add("div1")
-        div1.setAttribute("style", "outline: 1px solid; display:flex; flex-direction: column; background-color: pink; height: 100%; width: 100%;")
+        div1.setAttribute("style", "outline: 1px solid; display:flex; flex-direction: column; background-color: white; height: 100%; width: 100%;")
         for(let i=0; i<gridSize; i++){
             const div2 = document.createElement("div");
             div2.classList.add("div2");
-            div2.setAttribute("style", "outline: 1px solid; display:flex; flex-direction: column; background-color: pink; height: 100%; width: 100%;")
+            div2.setAttribute("style", "outline: 1px solid; display:flex; flex-direction: column; background-color: white; height: 100%; width: 100%;")
             div1.appendChild(div2)
         }
         container.appendChild(div1)
